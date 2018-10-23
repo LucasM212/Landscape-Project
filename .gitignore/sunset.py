e746135bd1754f925b23a579116baef9f0163bcb
LucasM212 <43610120+LucasM212@users.noreply.github.com>
2018-10-23 09:38:56 -0400

    Update sunset.py

--- .gitignore/sunset.py
+++ .gitignore/sunset.py
@@ -1,6 +1,8 @@
 height2 = -100
 x = 320
 x2 = -100
+x3 = 640
+height3 = 240
 
 def setup():
     size(640, 480)
@@ -10,14 +12,20 @@ def draw():
     global x
     global height2
     global x2
+    global x3
+    global height3
     
-    if height2 >= 480:
+    if height2 >= 640:
         height2 = -100
-    height2 += 2
+    height2 += 1
     
-    if x2 >= 640:
+    if x2 >= 800:
         x2 = -100
-    x2 += 2
+    x2 += 1
+    
+    if x3 <= -100:
+        x3 = 640
+    x3 -= 1
     
     background(139, 90, 0)
     
@@ -29,3 +37,8 @@ def draw():
     ellipse(x2, height/2, 50, 50)
     ellipse(x2+30, height/2, 50, 50)
     ellipse(x2+10, height/2-20, 50, 50)
+    
+    fill(255, 255, 255)
+    ellipse(x3, height3, 50, 50)
+    ellipse(x3+30, height3, 50, 50)
+    ellipse(x3+10, height3-20, 50, 50)
